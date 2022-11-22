bd1186962b95df1d3ef3946d6d4427cb2b053b56
Adriano Singolani <adrianosingolani@gmail.com>
2022-11-22 20:26:10 -0300

    test

--- components/Home.js
+++ components/Home.js
@@ -2,10 +2,14 @@ import styles from '../styles/Layout.module.css'
 
 function Home() {
     return (
+        <>
         <h2>
             <span className={styles.avoidwrap}>I design beautiful, simple things&nbsp;</span>
             <span className={styles.avoidwrap}>and I love what I do</span>
         </h2>
+        
+        <h3>test</h3>
+        </>
     )
 }
 
