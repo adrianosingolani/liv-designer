476e7115fc4c52e28753b4df35488ef1a17cd307
adrianosingolani <adrianosingolani@gmail.com>
2022-11-22 23:31:37 +0000

    Deploying to gh-pages from @ adrianosingolani/liv-designer@594d176a28f55ee2580d59694f657a67ce144a34 🚀

---
+++ _next/static/qqz41dz_5-8jA5tQzTiUo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/40b36217389df4b9.css","static/chunks/pages/index-11807dd73fc5b2de.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
